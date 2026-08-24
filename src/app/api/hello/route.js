e5c824323a308fbf8291d74ca87bcd229879487c
minhnhat-cyber <u6632058@au.edu>
2026-08-24 11:17:46 +0700

    update

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -1,9 +1,19 @@
 import { NextResponse } from "next/server";
+import corsHeaders from "@/lib/cors";
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 200,
+    headers: corsHeaders,
+  });
+}
 
 export async function GET() {
   const message = {
     message: "hello world",
   };
 
-  return NextResponse.json(message);
+  return NextResponse.json(message, {
+    headers: corsHeaders,
+  });
 }
